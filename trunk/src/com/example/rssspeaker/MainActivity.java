91b9cf3bd2e04b3d04547301901b9d29cefe2ba5
germanoa@gmail.com <germanoa@gmail.com@5c43ee99-6ca3-6135-0202-2c6d49014db4>
2013-06-09 21:27:24 +0000

    stt ok

--- trunk/src/com/example/rssspeaker/MainActivity.java
+++ trunk/src/com/example/rssspeaker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rssspeaker;
 
+import java.util.ArrayList;
 import java.util.Locale;
  
 import android.app.Activity;
@@ -7,16 +8,24 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.content.ActivityNotFoundException;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
 
 	/* Called when the activity is first created. */
 
-	
+	protected static final int RESULT_SPEECH = 1;
+	 	
     private TextToSpeech tts;
     private Button btnSpeak;
+    private ImageButton btnCapture;
     private EditText txtText;
     
     @Override
@@ -27,17 +36,59 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         tts = new TextToSpeech(this, this);
     
         btnSpeak = (Button) findViewById(R.id.btnSpeak);
-    
+        btnCapture = (ImageButton) findViewById(R.id.btnCapture);    
         txtText = (EditText) findViewById(R.id.txtText);
-    
-        // button on click event
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-    
+
+        
+        OnClickListener menuOnClickListener = new OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                speakOut();
+            public void onClick(final View arg0) {
+            	 switch(arg0.getId()){
+                 	case R.id.btnSpeak:
+                 		speakOut();
+                 		break;
+                 	case R.id.btnCapture:
+                 		 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+          
+                         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+          
+                         try {
+                             startActivityForResult(intent, RESULT_SPEECH);
+                             txtText.setText("");
+                         } catch (ActivityNotFoundException a) {
+                             Toast t = Toast.makeText(getApplicationContext(),
+                                     "Opps! Your device doesn't support Speech to Text",
+                                     Toast.LENGTH_SHORT);
+                             t.show();
+                         }
+                     break;
+            	 }
             }
-        });
+        };
+        
+        // button on click event
+        btnSpeak.setOnClickListener(menuOnClickListener);
+        btnCapture.setOnClickListener(menuOnClickListener);
+        
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+ 
+        switch (requestCode) {
+        case RESULT_SPEECH: {
+            if (resultCode == RESULT_OK && null != data) {
+ 
+                ArrayList<String> text = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+ 
+                txtText.setText(text.get(0));
+            }
+            break;
+        }
+ 
+        }
     }
     
     @Override
