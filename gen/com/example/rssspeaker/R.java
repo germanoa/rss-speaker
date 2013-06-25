9e24b6e5071018474dde9c1b761184fdec4baef5
germanoa@gmail.com <germanoa@gmail.com@5c43ee99-6ca3-6135-0202-2c6d49014db4>
2013-06-25 06:49:23 +0000

    versao 0.1

--- gen/com/example/rssspeaker/R.java
+++ gen/com/example/rssspeaker/R.java
@@ -11,8 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int blue=0x7f060000;
-        public static final int red=0x7f060001;
+        public static final int blue=0x7f060001;
+        public static final int blue_facebook=0x7f060000;
+        public static final int red=0x7f060002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -25,16 +26,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int download=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f090006;
         public static final int btnCapture=0x7f090003;
         public static final int btnLogin=0x7f090000;
         public static final int btnSpeak=0x7f090004;
-        public static final int txtPass=0x7f090002;
+        public static final int txtPass=0x7f090001;
         public static final int txtText=0x7f090005;
-        public static final int txtUser=0x7f090001;
+        public static final int txtUser=0x7f090002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
